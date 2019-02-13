2b553b6b46bf7d0ced91c21be496f0860ee4ed6c
gunnarmein <gmein@eastsideprep.org>
2019-02-12 21:21:11 -0800

    p2 stuff

--- P2/OO/src/oo/OO.java
+++ P2/OO/src/oo/OO.java
@@ -5,6 +5,12 @@
  */
 package oo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
 /**
  *
  * @author gmein
@@ -17,20 +23,98 @@ public class OO {
     public static void main(String[] args) {
         EPSPerson p = new EPSPerson("Gunnar Mein", "gmein@eastsideprep.org");
         System.out.println(p);
-        
-        Student s = new Student ("Bohan", "Shakes", 9);
+
+        Student s = new Student("Bohan", "Shakes", 9);
         System.out.println(s);
-        
+
         MrOwen mo1 = new MrOwen();
-        System.out.println("We made an object "+mo1.hashCode());
-        mo1.setHobby("Biking");  
+        System.out.println("We made an object " + mo1.hashCode());
+        mo1.setHobby("Biking");
         MrOwen mo2 = new MrOwen();
-        System.out.println("We made an object "+mo2.hashCode());
+        System.out.println("We made an object " + mo2.hashCode());
         mo2.setHobby("SeeaaaaaaHawks");
-        
+
         System.out.println(mo1.getHobby());
         System.out.println(mo2.getHobby());
-        
+
+        //
+        // Collections
+        //
+        ArrayList<String> list = new ArrayList<>(); // when you first type this,an error appears - click the lightbulb and import the class
+        list.add("a");
+        list.add("word");
+        list.add("another");
+        list.add("word");
+
+        // sort the list, using the natural order of type String
+        list.sort(null);
+
+        // copy one list to another list
+        ArrayList<String> list2 = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            list2.add(list.get(i));
+        }
+
+        // copy one list to another list, leaving out anything that says "word"
+        list2.clear();
+        for (int i = 0; i < list.size(); i++) {
+            String item = list.get(i);
+            if (!s.equals("word")) {
+                list2.add(item);
+            }
+        }
+
+        // I can make ArrayLists of other items, too - like tuples
+        Tuple t1 = new Tuple("hah", 1);
+        Tuple t2 = new Tuple("bah", 2);
+
+        ArrayList<Tuple> tlist = new ArrayList<>();  // I don't have to say "Tuple " on the right side because it is clear from the left side
+        tlist.add(t1);
+        tlist.add(t2);
+
+        // and of course I can sort:
+        tlist.sort(null);
+        // what would the "natural sort order" be for a tuple? I am not giving a Comparator as an argument, so it will look for 
+        // the "Comparable" interface on the Tuple class. Luckily, you are providing that. 
+
+        ArrayList<String> speech = new ArrayList<>();
+        speech.add("a");
+        speech.add("b");
+        speech.add("a");
+        speech.add("c");
+        speech.add("c");
+        speech.add("b");
+
+        ArrayList<String> useless = new ArrayList<>();
+        useless.add("b");
+        useless.add("c");
+
+        System.out.println(speech);
+
+//        for (int i = 0; i < useless.size(); i++) {
+//
+//            if (speech.contains(useless.get(i))) {
+//
+//                speech.remove(useless.get(i));
+//
+//                i--;
+//
+//            }
+//
+//        }
+        for (int i = 0; i < useless.size(); i++) {
+// if (speech.contains("\\s"+useless.get(i)+"\\s")) {
+
+            if (speech.contains(useless.get(i))) {
+                speech.remove(useless.get(i));
+
+                i--;
+
+            }
+        }
+
+        System.out.println(speech);
+
     }
-    
+
 }
--- P2/OO/src/oo/Tuple.java
+++ P2/OO/src/oo/Tuple.java
@@ -11,8 +11,21 @@ package oo;
  */
 public class Tuple implements Comparable<Tuple>{
 
+    int count;
+    String word;
+
+    // constructors make the code look niceer when you use this class
+    Tuple(String word, int count) {
+        this.count = count;
+        this.word = word;
+    }
+    
     @Override
     public int compareTo(Tuple arg0) {
+        // you have to write code that compares "this" to "arg0", and return negative, 0, or positive
+        // remember you are supposed to compare to tuples based on their count, not on their words.
+        // for those wanting a challenge, implement this so that it sub-sorts by words if the counts are equal.
+        return 0;
     }
     
 }
